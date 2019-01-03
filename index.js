5a1a70c5c1875a931e74595f5195b0eec9a9b948
Armaiz <armaiz.adenwala@gmail.com>
2019-01-02 16:31:33 -0800

    add files

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { Provider } from "react-redux";
+import configureStore from "./configureStore";
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
